5667fb4cc6344b86e616298f137fb57dd127f2de
YMB06 <youssefmf240@gmail.com>
2025-03-06 20:05:36 +0100

    inicio

---
+++ porfolio/src/main/java/com/youssef/porfolio/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.youssef.porfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+/*esta clase de java va a ser un controlador, por ser controlador su funcion exclusiva
+es recibir peticiones http tipo GET, PATCH, POST, DELETE*/ 
+// Framework --> mvc, decoradores
+// el decorador del framework()SPB @controller dota a la clase para recibir peticiones HTTP
+@Controller
+public class HomeController {
+    // con este decorador tomcat va a ejecutar la raiz del sitio 
+    // y va a interpretar el index.html
+    @GetMapping("/")
+    public String home(){
+        return "/www/gamas/index";
+    }
+    
+    @GetMapping("/i")
+    public String qSomos(){
+        return "/www/site/index";
+    }
+    
+    @GetMapping("/Contacto")
+    public String contacto(){
+        return "/www/site/Contacto";
+    }
+}
